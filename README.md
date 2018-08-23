793bcdcc0677484280bcce0065d93afb24d7486a
abhilashsajeev <abhilashsajeev@gmail.com>
2018-08-23 12:59:56 +0530

    Adding redux logger and running instructions

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # todo-card
 Simple todo application built in React redux
+
+## How to run the app
+
+SET NODE_ENV to development
+
+
+```
+$ cd todo-card
+$ npm i
+
+```
+
+To start the application
+
+```
+$ npm start
+```
+
+To run the tests
+
+```
+npm test
+```
--- todo-card/src/store.js
+++ todo-card/src/store.js
@@ -2,11 +2,15 @@ import {createStore, applyMiddleware, compose, combineReducers} from 'redux';
 import thunk from 'redux-thunk';
 import todoReducer from './reducers/todo';
 import messageReducer from './reducers/message';
+import { createLogger } from 'redux-logger'
+
+const logger = createLogger();
 
 const reducer = combineReducers({
   todos: todoReducer,
   message: messageReducer
 })
 
+const middlewares = [thunk, logger]
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-export default createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
+export default createStore(reducer, composeEnhancers(applyMiddleware(...middlewares)));
